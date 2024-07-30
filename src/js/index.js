d7e1c3b7f05d6856e8dd7d4f24799ba34464a2ce
Ewerson Roldao Cardoso <ewwerson@gmail.com>
2024-07-29 21:20:30 -0300

    Atualizando CSS e JS
    Colocando links e arrumando CSS

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,47 @@
 function SpanMsg(){
-    return alert('Clicou')
+    var searchBarId = document.getElementById('searchBarId');
+    var valorSearchBarId = searchBarId.value;
+    var whatsApp = 'https://api.whatsapp.com/send?phone=554836230198&text='+valorSearchBarId;
+    window.open(whatsApp)
 };
+
+function Email(){
+    var email = 'https://api.whatsapp.com/send?phone=5548998010108&text=Quero+mais+informações+sobre+materiais+de+Pesca'
+    window.open(email)
+
+}
+
+function PregoLisoCCPequenoPage(){
+    var pregoLisoCCPequenoPage = 'https://www.nautfix.com.br/MLB-3781913279-prego-galvanizado-fogo-deck-naval-barco-1kg-liso-pequeno-_JM'
+    window.open(pregoLisoCCPequenoPage)
+}
+
+function PregoLisoCCPage(){
+    var pregoLisoCCPage = 'https://www.nautfix.com.br/MLB-4907565100-prego-galvanizado-fogo-naval-deck-barco-1kg-liso-c-cabeca-_JM'
+    window.open(pregoLisoCCPage)
+}
+
+function PregoLisoCCGrandePage(){
+    var pregoLisoCCGrandePage = 'https://www.nautfix.com.br/MLB-3782084039-prego-galvanizado-fogo-deck-naval-barco-1kg-liso-grandes-_JM'
+    window.open(pregoLisoCCGrandePage)
+}
+
+function PregoLisoSCPage(){
+    var pregoLisoSCPage = 'https://www.nautfix.com.br/MLB-4913404934-prego-galvanizado-a-fogo-sem-cabeca-deck-pergolado-barco-1kg-_JM'
+    window.open(pregoLisoSCPage)
+}
+
+function PregoArdoxPage(){
+    var pregoArdoxPage = 'https://www.nautfix.com.br/MLB-4909400292-prego-galvanizado-a-fogo-ardox-deck-pergolado-barco-1kg-_JM'
+    window.open(pregoArdoxPage)
+}
+
+function PregoAneladoPage(){
+    var pregoAneladoPage = 'https://www.nautfix.com.br/MLB-4909686630-prego-galvanizado-a-fogo-anelado-deck-pergolado-barco-1kg-_JM'
+    window.open(pregoAneladoPage)
+}
+
+function PregoLisoSC12x12(){
+    var pregoLisoSC12x12 = 'https://www.nautfix.com.br/MLB-3783841387-prego-galvanizado-a-fogo-12x12-sem-cabeca-deck-barco-1kg-_JM'
+    window.open(pregoLisoSC12x12)
+}

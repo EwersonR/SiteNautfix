f04754ecfa151a49a863e63b8cd8240fda3d9547
Ewerson Roldao Cardoso <ewwerson@gmail.com>
2024-08-01 20:23:46 -0300

    adicionado colas navais

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,16 @@ function Email(){
 
 }
 
+function PesquisaPrego(){
+    var pesquisaPrego = 'https://www.nautfix.com.br/pregos'
+    window.open(pesquisaPrego)
+}
+
+function PesquisaColaNaval(){
+    var pesquisaColaNaval = 'https://www.nautfix.com.br/cola%20naval'
+    window.open(pesquisaColaNaval)
+}
+
 function PregoLisoCCPequenoPage(){
     var pregoLisoCCPequenoPage = 'https://www.nautfix.com.br/MLB-3781913279-prego-galvanizado-fogo-deck-naval-barco-1kg-liso-pequeno-_JM'
     window.open(pregoLisoCCPequenoPage)
@@ -44,4 +54,29 @@ function PregoAneladoPage(){
 function PregoLisoSC12x12(){
     var pregoLisoSC12x12 = 'https://www.nautfix.com.br/MLB-3783841387-prego-galvanizado-a-fogo-12x12-sem-cabeca-deck-barco-1kg-_JM'
     window.open(pregoLisoSC12x12)
+}
+
+function ColaNavalKit4kg(){
+    var colaNavalKit4kg = 'https://www.nautfix.com.br/MLB-4887057384-cola-bi-componente-epoxi-naval-kit-4kg-_JM'
+    window.open(colaNavalKit4kg)
+}
+
+function ColaNavalKit2kg(){
+    var colaNavalKit2kg = 'https://www.nautfix.com.br/MLB-3553926668-cola-naval-epoxi-nautfix-2kg-conjunto-resina-e-endurecedor-_JM'
+    window.open(colaNavalKit2kg)
+}
+
+function ColaNavalKit800gr(){
+    var colaNavalKit800gr = 'https://www.nautfix.com.br/MLB-3774148583-cola-naval-bi-componente-epoxi-naval-800g-_JM'
+    window.open(colaNavalKit800gr)
+}
+
+function ColaNavalKit400gr(){
+    var colaNavalKit400gr = 'https://www.nautfix.com.br/MLB-3774219945-cola-naval-bi-componente-epoxi-naval-400g-_JM'
+    window.open(colaNavalKit400gr)
+}
+
+function ColaNavalKit200gr(){
+    var colaNavalKit200gr = 'https://www.nautfix.com.br/MLB-3774249543-cola-naval-bi-componente-epoxi-naval-200g-_JM'
+    window.open(colaNavalKit200gr)
 }
